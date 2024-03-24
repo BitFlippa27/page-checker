fe9db901cbfbebfa290d0bdf1076e0c3c5a62b3d
y <dennis.erdelean@protonmail.com>
2024-03-24 19:07:46 +0100

    save to db works

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const http = require('http');
-const { cronJob } = require('./main.js');
+const mongoose = require('mongoose');
+const timeScheduler = require('./app.js');
 
-const server = http.createServer(async (req, res) => {
-  res.end('Server is running...');
-  cronJob.start();
+const MONGO_URL = "mongodb+srv://bitflippa:vI11Lcmr6AHSPQ7q@cluster0.i5cwt1d.mongodb.net/page-checker?retryWrites=true&w=majority&appName=Cluster0";
+
+
+
+mongoose.connection.on('open', () => {
+  console.log(`MongoDB connection ready`);
 });
 
-server.listen(7777, () => {
-  console.log('Server is running on port 7777');
+mongoose.connection.on('error', (error) => {
+  console.error(`MongoDB connection error: ${error.message}`);
 });
 
-server.on('error', (error) => {
-  console.error(`Error: ${error.message}`);
-});
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    const server = http.createServer(async (req, res) => {
+      res.end('Server is running...');
+     
+      timeScheduler.start();
+    });
+    server.listen(7777, () => {
+      console.log('Server is running on port 7777');
+    });
+    server.on('error', (error) => {
+      console.error(`Error: ${error.message}`);
+    });
+  } catch (error) {
+    console.error(`Error in startServer: ${error.message}`);
+  }
+}
+
+startServer();
+
+
+
